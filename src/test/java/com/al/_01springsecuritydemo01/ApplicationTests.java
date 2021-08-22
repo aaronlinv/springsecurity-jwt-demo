722edd756c17e1a2cdc0d372178ce65af7f69e59
AaronLin <1546848781@qq.com>
2021-08-22 13:53:40 +0800

    init

---
+++ src/test/java/com/al/_01springsecuritydemo01/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.al._01springsecuritydemo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
