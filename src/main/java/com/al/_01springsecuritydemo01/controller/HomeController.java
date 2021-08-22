a3653a02ab9e074d0e25387cf387b2b338353d8d
AaronLin <1546848781@qq.com>
2021-08-22 14:09:47 +0800

    页面

---
+++ src/main/java/com/al/_01springsecuritydemo01/controller/HomeController.java
@@ -0,0 +1,35 @@
+package com.al._01springsecuritydemo01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    // http://localhost:8080
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+
+    // 管理
+    @GetMapping("/system/user")
+    public String userList() {
+        return "user";
+    }
+
+    @GetMapping("/system/role")
+    public String roleList() {
+        return "role";
+    }
+
+    @GetMapping("/system/menu")
+    public String menu() {
+        return "menu";
+    }
+
+    @GetMapping("/order")
+    public String orderList() {
+        return "order";
+    }
+}
